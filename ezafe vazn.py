81ee7b5b7644834f94e51604c58252686847102c
Amir hossein Mousavi <amir.homos@gmail.com>
2019-08-17 06:56:05 +0430

    BMI
    BMI cheking

---
+++ ezafe vazn.py
@@ -0,0 +1,14 @@
+weight = float(input())
+height = float(input())
+
+bmi = weight/height**2
+
+print("%.2f"%(bmi))
+if bmi < 18.5 :
+    print('Underweight')
+elif 18.5 <= bmi < 25 :
+    print('Normal')
+elif 25 <= bmi < 30 :
+    print('Overweight')
+elif 30 <= bmi :
+    print('Obese')
